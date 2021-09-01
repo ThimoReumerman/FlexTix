b1dda5f11ec3936e03239256fb86fa200f334b4c
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2021-09-01 14:39:09 +0200

    Updated artists.ts

--- server/src/routes/artists.ts
+++ server/src/routes/artists.ts
@@ -1,17 +1,35 @@
 import express, {Application, Request, Response, NextFunction, Router, request} from 'express';
-import connection from '../mongoose';
 
 // Import Artist model
 import Artist, {IArtist} from "../models/Artist";
-import mongoose from '../mongoose';
 
 var router: Router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-    res.status(200).send("Get");
-    mongoose.get("artists")
+// Get all artists
+router.get("/", async (req: Request, res: Response) => {
+    try {
+        const artists: IArtist[] = await Artist.find();
+    
+        return res.status(200).json(artists);
+    } catch (err) {
+        return res.status(400).send("Something went wrong.");
+    }
 });
 
+// Get one artist
+router.get("/:_id", async (req: Request, res: Response) => {
+
+    // Try getting one artist from the database
+    try {
+        const artist: IArtist[] = await Artist.findOne({_id: req.params._id});
+    
+        return res.status(200).json(artist);
+    } catch (err) {
+        return res.status(400).send("Something went wrong.");
+    }
+});
+
+// Create new artist
 router.post("/", async (req: Request, res: Response) => {
 
     // Create new artist variable
@@ -21,9 +39,9 @@ router.post("/", async (req: Request, res: Response) => {
     try {
         const savedArtist = await artist.save();
     
-        res.sendStatus(200).json(savedArtist);
+        return res.sendStatus(200).json(savedArtist);
     } catch (err) {
-        res.sendStatus(400).send("Couldn't save artist.")
+        return res.sendStatus(400).send("Couldn't save artist.")
     }
 });
 
