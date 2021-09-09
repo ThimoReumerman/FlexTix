5a48b3ef8d1f4ae76f698efa21e529eaa7e765ea
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2021-09-09 10:53:13 +0200

    Updated artist form from class to functional component

--- client/src/components/forms/ArtistForm.tsx
+++ client/src/components/forms/ArtistForm.tsx
@@ -1,118 +1,134 @@
-import { Component } from 'react';
+import { Component, useState, useEffect } from "react";
 
-import axios, { AxiosResponse } from 'axios';
-import apiLink from '../../api';
+import axios, { AxiosResponse } from "axios";
+import apiLink from "../../api";
 
-import IArtist, {EmptyArtist} from "../../interfaces/Artist";
-import IMedia, {EmptyMedia} from "../../interfaces/Media";
+import IArtist, { EmptyArtist } from "../../interfaces/Artist";
+import IMedia, { EmptyMedia } from "../../interfaces/Media";
+
+import { useForm, SubmitHandler } from "react-hook-form";
 
 import MediaForm from "../forms/MediaForm";
 
 interface IProps {
-    _id?: string
+  _id?: string;
 }
 
-interface IState {
-    artist?: IArtist,
-    media: IMedia[]
-}
+const ArtistForm: React.FC<IProps> = ({ _id }) => {
+  const [artist, setArtist] = useState<IArtist>();
+  const [media, setMedia] = useState<IMedia[]>();
 
-class ArtistForm extends Component<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
+  const { register, handleSubmit } = useForm<IArtist>();
 
-        this.state = ({
-            artist: EmptyArtist,
-            media: new Array<IMedia>()
-        })
-    }
-
-    async componentDidMount() {
-
-        console.log("ArtistForm component did mount.");
-
-        this.addMedia.bind(this);
-        this.handleChange.bind(this);
-
+  useEffect(() => {
+    const initialize = async () => {
+        console.log("Initializing form...");
+    
         let exists: boolean = false;
-
+    
         // Get artist if ID exists
-        if (this.props._id) {
-
-            // Fetch artist data
-            const artistResponse: AxiosResponse<IArtist> = await axios.get(`${apiLink}/artists/${this.props._id}`);
-
-            // Return if artist data is null
-            if (artistResponse.data == null) return;
-
-            // If not null, you can say the artist exists
-            exists = true;
-
-            // Set state of artist to fetched data
-            this.setState({artist: artistResponse.data});
-
-            // Create media array
-            let mediaArray: IMedia[] = new Array<IMedia>();
-
-            // Loop through media items to get the actual media information
-            artistResponse.data.media?.forEach(async (media) => {
-
-                // Fetch media data from database
-                const mediaResponse: AxiosResponse<IMedia> = await axios.get(`${apiLink}/media/${media.mediaId}`);
-
-                // Push new media data to media array
-                mediaArray.push(mediaResponse?.data);
-            });
-
-            if (mediaArray.length === 0) mediaArray.push(EmptyMedia);
-
-            // Set the new media array in the media state
-            this.setState({media: mediaArray});
-
+        if (_id) {
+          // Fetch artist data
+          const artistResponse: AxiosResponse<IArtist> = await axios.get(
+            `${apiLink}/artists/${_id}`
+          );
+    
+          // Return if artist data is null
+          if (artistResponse.data == null) return;
+    
+          // If not null, you can say the artist exists
+          exists = true;
+    
+          // Set state of artist to fetched data
+          setArtist(artistResponse.data);
+    
+          // Create media array
+          let mediaArray: IMedia[] = new Array<IMedia>();
+    
+          // Loop through media items to get the actual media information
+          artistResponse.data.media?.forEach(async (media) => {
+            // Fetch media data from database
+            const mediaResponse: AxiosResponse<IMedia> = await axios.get(
+              `${apiLink}/media/${media.mediaId}`
+            );
+    
+            // Push new media data to media array
+            mediaArray.push(mediaResponse?.data);
+          });
+    
+          if (mediaArray.length === 0) mediaArray.push(EmptyMedia);
+    
+          // Set the new media array in the media state
+          setMedia(mediaArray);
         }
-
+    
         if (!exists) {
-            this.setState({artist: EmptyArtist});
+          setArtist(EmptyArtist);
         }
-    }
+      }
 
-    addMedia (media: IMedia) {
-        console.log(`Adding media with ID ${media._id}`)
-        
-        let newMedia: IMedia[] = this.state!.media!;
+      initialize();
+  });
 
-        newMedia.push(media);
+  const onSubmit: SubmitHandler<IArtist> = (data) => {
 
-        this.setState({media: newMedia});
-    }
+  }
 
-    handleChange (e: React.FormEvent<HTMLFormElement>) {
+  const addMedia = (m: IMedia) => {
+    // Log message to console
+    console.log(`Adding media with ID ${m._id}`);
 
-    }
+    // Add new media array
+    let newMedia: IMedia[] = media!;
 
-    render() {
+    // Remove last media item (which is empty)
+    newMedia.pop();
+
+    // Add new media item
+    newMedia.push(m);
+
+    // Set media array state to the new array
+    setMedia(newMedia);
+  };
+
+  return (
+    <div>
+      <form id="artistForm" onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Name
+          <input
+            id="artistName"
+            type="text"
+            name="name"
+            defaultValue={artist?.name}
+          />
+        </label>
+        <label>
+          Bio
+          <textarea id="artistBio" name="bio" defaultValue={artist?.bio} />
+        </label>
+        <label>
+          Submit
+          <input id="submitArtist" type="submit" />
+        </label>
+      </form>
+      {media?.map((m) => {
         return (
-            <div>
-                <form id="artistForm" onChange={(e) => {this.handleChange(e)}}>
-                    <label htmlFor="artistName">
-                        Name
-                        <input id="artistName" type="text" name="name" defaultValue={this.state.artist?.name} />
-                    </label>
-                    <label htmlFor="artistBio">
-                        Bio
-                        <textarea id="artistBio" name="bio" defaultValue={this.state?.artist?.bio} />
-                    </label>
-                </form>
-                {
-                        this.state?.media?.map((m) => {
-                            return <MediaForm media={m} onMediaSubmit={(media: IMedia) => this.addMedia(media)} />
-                    })
-                }
-                <button onClick={() => {this.addMedia(EmptyMedia)}}>Add media</button>
-            </div>
-
-        )
-    }
-}
+          <MediaForm
+            media={m}
+            onMediaSubmit={(media: IMedia) => addMedia(media)}
+          />
+        );
+      })}
+      <button
+        onClick={() => {
+          addMedia(EmptyMedia);
+        }}
+      >
+        Add media
+      </button>
+    </div>
+  );
+};
 
 export default ArtistForm;
